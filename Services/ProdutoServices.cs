97a3ae14120196efd2bf8e332fa1162de93225f0
luan048 <luanmonteiropedrosa@gmail.com>
2025-08-13 19:39:29 -0300

    feat: criando método de cadastrar usuários no Services

--- Services/ProdutoServices.cs
+++ Services/ProdutoServices.cs
@@ -19,5 +19,33 @@ namespace CadastroClient_ASP.Net_SqlServer.Services
         {
             return await _context.Produto.ToListAsync();
         }
+
+        public async Task<ProdutoModel> CreateUserAsync()
+        {
+            Console.Write("Informe o nome :");
+            string nome = Console.ReadLine()!;
+
+            Console.Write("Informe o email: ");
+            string email = Console.ReadLine()!;
+
+            Console.Write("Informe a senha: ");
+            string password = Console.ReadLine()!;
+
+            Console.Write("Informe a data de nascimento: ");
+            string DataNascimento = Console.ReadLine()!;
+
+            var usuario = new ProdutoModel
+            {
+                Nome = nome,
+                Email = email,
+                Senha = password,
+                DataNascimento = DataNascimento
+            };
+
+            await _context.AddAsync(usuario);
+            await _context.SaveChangesAsync();
+
+            return usuario;
+        }
     }
 }
