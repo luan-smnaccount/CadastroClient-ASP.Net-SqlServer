0bc7f38b7577b3936261efda881db2830a4965af
luan-smnaccount <luansmn2605@gmail.com>
2025-09-01 09:21:33 -0300

    fix: adicionando propriedade nullable no DTO

--- DTO/UserUpdateDTO.cs
+++ DTO/UserUpdateDTO.cs
@@ -2,9 +2,9 @@ namespace DTO
 {
     public class UserUpdateDTO
     {
-        public string NovoNome { get; set; }
-        public string NovoEmail { get; set; }
-        public string NovaSenha { get; set; }
-        public string NovaDataNascimento { get; set; }
+        public string? NovoNome { get; set; }
+        public string? NovoEmail { get; set; }
+        public string? NovaSenha { get; set; }
+        public string? NovaDataNascimento { get; set; }
     }
 }
