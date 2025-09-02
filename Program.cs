38fb7f11740ce7bc5e50e3a0ff1cdb7589188710
luan-smnaccount <luansmn2605@gmail.com>
2025-09-02 14:15:05 -0300

    fix: removendo comentários do Program.cs

--- Program.cs
+++ Program.cs
@@ -5,30 +5,24 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Carregar variáveis de ambiente (.env)
 Env.Load();
 
-// Pega a string de conexão da variável de ambiente
 var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
 builder.Configuration["ConnectionStrings:DefaultConnection"] = connectionString;
 
-// Adiciona serviços MVC e Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Registra o contexto do EF Core com SQL Server
 builder.Services.AddDbContext<AppDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-// Registra o serviço ProdutoServices para injeção de dependência
 builder.Services.AddScoped<ProdutoServices>();
 
 var app = builder.Build();
 
-// Middleware Swagger para ambiente de desenvolvimento
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -36,9 +30,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthentication();
-
 app.MapControllers();
-
 app.Run();
